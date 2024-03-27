81841e8b4f179ab7c185b3d3fa31e2d0b4e47c23
xGrz <33755434+xGrz@users.noreply.github.com>
2024-03-27 12:16:11 +0100

    Fixed spacing in pagination

--- resources/views/components/pagination/types/desktop.blade.php
+++ resources/views/components/pagination/types/desktop.blade.php
@@ -3,11 +3,11 @@
         <div class="flex items-center justify-between">
             {{-- Previous Page Link --}}
             @if ($paginator->onFirstPage())
-                <x-payu::pagination.items.disabled class="p-1 mr-[.1rem] border border-slate-700 rounded-l-md">
+                <x-payu::pagination.items.disabled class="p-1 mr-[1px]">
                     <x-payu::icons.left-arrow />
                 </x-payu::pagination.items.disabled>
             @else
-                <x-payu::pagination.items.active href="{{ $paginator->previousPageUrl() }}" class="p-1 mr-[.1rem] border border-slate-700 rounded-l-md">
+                <x-payu::pagination.items.active href="{{ $paginator->previousPageUrl() }}" class="p-1 mr-[1px] rounded-l-md">
                     <x-payu::icons.left-arrow />
                 </x-payu::pagination.items.active>
             @endif
@@ -16,7 +16,7 @@
             {{-- Pagination Elements --}}
             @foreach ($elements as $element)
                 @if (is_string($element))
-                    <x-payu::pagination.items.disabled class="px-2 py-1 border border-transparent">
+                    <x-payu::pagination.items.disabled class="px-2 py-1">
                         {{ $element }}
                     </x-payu::pagination.items.disabled>
                 @endif
@@ -24,11 +24,11 @@
                 @if (is_array($element))
                     @foreach ($element as $page => $url)
                         @if ($page == $paginator->currentPage())
-                            <x-payu::pagination.items.disabled class="px-2 py-1 mr-[.1rem] border border-slate-700 !text-slate-300 !bg-slate-500">
+                            <x-payu::pagination.items.disabled class="px-2 py-1 mr-[1px] !text-slate-300 !bg-slate-500">
                                 {{ $page }}
                             </x-payu::pagination.items.disabled>
                         @else
-                            <x-payu::pagination.items.active href="{{$url}}" class="px-2 py-1 mr-[.1rem] border border-slate-700">
+                            <x-payu::pagination.items.active href="{{$url}}" class="px-2 py-1 mr-[1px]">
                                 {{ $page }}
                             </x-payu::pagination.items.active>
                         @endif
@@ -38,11 +38,11 @@
 
 
             @if ($paginator->hasMorePages())
-                <x-payu::pagination.items.active href="{{ $paginator->nextPageUrl() }}" class="p-1 mr-[.1rem] border border-slate-700 rounded-r-md">
+                <x-payu::pagination.items.active href="{{ $paginator->nextPageUrl() }}" class="p-1 mr-[1px] rounded-r-md">
                     <x-payu::icons.right-arrow />
                 </x-payu::pagination.items.active>
             @else
-                <x-payu::pagination.items.disabled class="p-1 mr-[.1rem] border border-slate-700 rounded-r-md">
+                <x-payu::pagination.items.disabled class="p-1 mr-[1px]">
                     <x-payu::icons.right-arrow />
                 </x-payu::pagination.items.disabled>
             @endif
