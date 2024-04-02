b6f90240fcf338bee288822726ec00f4af59c0a4
xGrz <33755434+xGrz@users.noreply.github.com>
2024-04-02 11:30:07 +0200

    PayoutObserver removes error message when status is not CANCELED

--- src/Observers/PayoutObserver.php
+++ src/Observers/PayoutObserver.php
@@ -3,6 +3,7 @@
 namespace xGrz\PayU\Observers;
 
 
+use xGrz\PayU\Enums\PayoutStatus;
 use xGrz\PayU\Events\PayoutCreated;
 use xGrz\PayU\Models\Payout;
 
@@ -11,6 +12,14 @@ class PayoutObserver
     public function created(Payout $payout): void
     {
         PayoutCreated::dispatch($payout);
+    }
+
+    public function updating(Payout $payout): void
+    {
+        if ($payout->error && $payout->status !== PayoutStatus::CANCELED) {
+            $payout->error = null;
+        }
+
     }
 
 }
