54baa94de65b5e7d74bec47b045e531dc6dcc5a7
xGrz <33755434+xGrz@users.noreply.github.com>
2024-04-04 10:57:16 +0200

    Fixed getting config credentials by ConfigService.

--- src/Services/ConfigService.php
+++ src/Services/ConfigService.php
@@ -27,25 +27,28 @@ class ConfigService
         return $this;
     }
 
-    public function getClientId(): ?string
+    public function getClientId(): ?int
     {
-        return config('payu.use_sandbox', true)
-            ? config('payu.api.oAuthClientId', self::SANDBOX_CREDENTIALS['PAYU_O_AUTH_CLIENT_ID'])
-            : config('payu.api.oAuthClientId');
+        if (config('payu.use_sandbox', true)) {
+            return config('payu.api.oAuthClientId') ?? self::SANDBOX_CREDENTIALS['PAYU_O_AUTH_CLIENT_ID'];
+        }
+        return config('payu.api.oAuthClientId');
     }
 
-    public function getClientSecret(): string
+    public function getClientSecret(): ?string
     {
-        return config('payu.use_sandbox', true)
-            ? config('payu.api.oAuthClientSecret', self::SANDBOX_CREDENTIALS['PAYU_O_AUTH_CLIENT_SECRET'])
-            : config('payu.api.oAuthClientSecret');
+        if (config('payu.use_sandbox', true)) {
+            return config('payu.api.oAuthClientSecret') ?? self::SANDBOX_CREDENTIALS['PAYU_O_AUTH_CLIENT_SECRET'];
+        }
+        return config('payu.api.oAuthClientSecret');
     }
 
-    public function getSignatureKey(): string
+    public function getSignatureKey(): ?string
     {
-        return config('payu.use_sandbox', true)
-            ? config('payu.api.signatureKey', self::SANDBOX_CREDENTIALS['PAYU_SIGNATURE_KEY'])
-            : config('payu.api.signatureKey');
+        if(config('payu.use_sandbox', true)) {
+            return config('payu.api.signatureKey') ?? self::SANDBOX_CREDENTIALS['PAYU_SIGNATURE_KEY'];
+        }
+        return config('payu.api.signatureKey');
     }
 
     public function getShopId(): ?string
@@ -53,11 +56,12 @@ class ConfigService
         return config('payu.api.shopId');
     }
 
-    public function getMerchantPosId(): int
+    public function getMerchantPosId(): ?int
     {
-        return config('payu.use_sandbox', true)
-            ? config('payu.api.merchantPosId', self::SANDBOX_CREDENTIALS['PAYU_MERCHANT_POS_ID'])
-            : config('payu.api.merchantPosId');
+        if (config('payu.use_sandbox', true)) {
+            return config('payu.api.merchantPosId') ?? self::SANDBOX_CREDENTIALS['PAYU_MERCHANT_POS_ID'];
+        }
+        return config('payu.api.merchantPosId');
     }
 
     public function getServiceDomain(): string
