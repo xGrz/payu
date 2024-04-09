ca43e6b4587f842fcba7c5b551eb51921e130591
xGrz <33755434+xGrz@users.noreply.github.com>
2024-04-09 15:53:49 +0200

    Added test trait

---
+++ tests/Traits/WithTransaction.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Traits;
+
+use xGrz\PayU\Facades\TransactionWizard\Buyer;
+use xGrz\PayU\Facades\TransactionWizard\Delivery\Address;
+use xGrz\PayU\Facades\TransactionWizard\Delivery\PostalBox;
+use xGrz\PayU\Facades\TransactionWizard\Product;
+use xGrz\PayU\Facades\TransactionWizard\Products;
+
+trait WithTransaction
+{
+    private function getProducts()
+    {
+        return Products::make([
+            Product::make('Product 1', 100, 2),
+            Product::make('Product 2', 200,),
+            Product::make('Product 3', 400, .5)
+        ]);
+    }
+
+    private function getAddressDelivery(): Address
+    {
+        return Address::make(
+            'test@example.com',
+            'Jonathan Kovalsky',
+            '198765432',
+            'Krakow',
+            'Zakopianska 200/2',
+            '91-200',
+            'PL',
+        );
+    }
+
+    private function getPostalBoxDelivery(): PostalBox
+    {
+        return PostalBox::make(
+            'test2@example.com',
+            'Michael Novak',
+            '600500200',
+            'WA101',
+        );
+    }
+
+    private function getBuyer(): Buyer
+    {
+        return new Buyer('example@example.com', '987567192', 'John', 'Travolta', 'en', 199);
+    }
+
+}
