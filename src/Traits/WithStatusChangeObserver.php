ecbebe7f69d11d9bcdc9c3f3eff772a5078d8afc
xGrz <33755434+xGrz@users.noreply.github.com>
2024-04-10 15:38:03 +0200

    Changed methods to static.

--- src/Traits/WithStatusChangeObserver.php
+++ src/Traits/WithStatusChangeObserver.php
@@ -6,14 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 trait WithStatusChangeObserver
 {
-    private function whenStatusChangedTo(Model $model, $status, string $eventClassName): void
+    private static function whenStatusChangedTo(Model $model, $status, string $eventClassName): void
     {
         if ($model->status === $status && $model->isDirty('status')) {
             $eventClassName::dispatch($model);
         }
     }
 
-    private function clearErrorMessage(Model $model, $status): void
+    private static function clearErrorMessage(Model $model, $status): void
     {
         if ($model->error && $model->status !== $status) {
             $model->error = null;
