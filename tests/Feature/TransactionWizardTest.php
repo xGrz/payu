398e51872c49cd3a86d099912b8167c53bf53e5f
xGrz <33755434+xGrz@users.noreply.github.com>
2024-04-13 15:16:05 +0200

    Added tests for buyer, address/postalbox delivery in TransactionWizardTest.php

--- tests/Feature/TransactionWizardTest.php
+++ tests/Feature/TransactionWizardTest.php
@@ -110,6 +110,48 @@ class TransactionWizardTest extends TestCase
         $this->assertEquals('en', $buyer['language']);
     }
 
+    public function test_buyer_created_by_new_object_instance()
+    {
+        $buyer = new TransactionWizard\Buyer();
+        $buyer
+            ->setEmail('test@example.com')
+            ->setPhone('123456789')
+            ->setFirstName('Michael')
+            ->setLastName('Schumacher')
+            ->setLanguage('de')
+            ->setCustomerId(201);
+        $buyerArr = $buyer->toArray();
+
+        $this->assertIsArray($buyerArr);
+        $this->assertEquals(201, $buyerArr['extCustomerId']);
+        $this->assertEquals('test@example.com', $buyerArr['email']);
+        $this->assertEquals('123456789', $buyerArr['phone']);
+        $this->assertEquals('Michael', $buyerArr['firstName']);
+        $this->assertEquals('Schumacher', $buyerArr['lastName']);
+        $this->assertEquals('de', $buyerArr['language']);
+    }
+
+    public function test_buyer_created_by_static_make()
+    {
+        $buyer = TransactionWizard\Buyer::make(
+            'mv@example.com',
+            '1234567891',
+            'Max',
+            'Verstappen',
+            'nl',
+            22,
+        );
+        $buyerArr = $buyer->toArray();
+
+        $this->assertIsArray($buyerArr);
+        $this->assertEquals(22, $buyerArr['extCustomerId']);
+        $this->assertEquals('mv@example.com', $buyerArr['email']);
+        $this->assertEquals('1234567891', $buyerArr['phone']);
+        $this->assertEquals('Max', $buyerArr['firstName']);
+        $this->assertEquals('Verstappen', $buyerArr['lastName']);
+        $this->assertEquals('nl', $buyerArr['language']);
+    }
+
     public function test_address_delivery_definition()
     {
         $delivery = $this->getAddressDelivery()->toArray();
@@ -121,6 +163,87 @@ class TransactionWizardTest extends TestCase
         $this->assertEquals('Zakopianska 200/2', $delivery['street']);
         $this->assertEquals('91-200', $delivery['postalCode']);
         $this->assertEquals('PL', $delivery['countryCode']);
+    }
+
+    public function test_address_delivery_created_by_new_object_instance()
+    {
+        $address = new TransactionWizard\Delivery\Address();
+        $address
+            ->setRecipientEmail('jv@example.com')
+            ->setRecipientPhone('1234567891')
+            ->setRecipientName('Jost Verstappen')
+            ->setCity('Poznan')
+            ->setStreet('Warszawska 120/1')
+            ->setPostalCode('23-302')
+            ->setCountryCode('PL');
+
+        $addressArr = $address->toArray();
+        $this->assertIsArray($addressArr);
+        $this->assertEquals('Jost Verstappen', $addressArr['recipientName']);
+        $this->assertEquals('jv@example.com', $addressArr['recipientEmail']);
+        $this->assertEquals('1234567891', $addressArr['recipientPhone']);
+        $this->assertEquals('Warszawska 120/1', $addressArr['street']);
+        $this->assertEquals('Poznan', $addressArr['city']);
+        $this->assertEquals('23-302', $addressArr['postalCode']);
+        $this->assertEquals('PL', $addressArr['countryCode']);
+
+    }
+
+    public function test_address_delivery_created_by_static_make()
+    {
+        $address = TransactionWizard\Delivery\Address::make(
+            'mv@example.com',
+            'Max Verstappen',
+            '1234567891',
+            'Poznan',
+            'Warszawska 120/1',
+            '23-302',
+            'PL',
+        );
+        $addressArr = $address->toArray();
+        $this->assertIsArray($addressArr);
+        $this->assertEquals('Max Verstappen', $addressArr['recipientName']);
+        $this->assertEquals('mv@example.com', $addressArr['recipientEmail']);
+        $this->assertEquals('1234567891', $addressArr['recipientPhone']);
+        $this->assertEquals('Warszawska 120/1', $addressArr['street']);
+        $this->assertEquals('Poznan', $addressArr['city']);
+        $this->assertEquals('23-302', $addressArr['postalCode']);
+        $this->assertEquals('PL', $addressArr['countryCode']);
+
+    }
+
+    public function test_postal_box_delivery_created_by_new_object_instance()
+    {
+        $postalBox = new TransactionWizard\Delivery\PostalBox();
+        $postalBox
+            ->setRecipientEmail('jv@example.com')
+            ->setRecipientPhone('1234567891')
+            ->setRecipientName('Jost Verstappen')
+            ->setPostalBox('KR203');
+
+        $postalBoxArr = $postalBox->toArray();
+        $this->assertIsArray($postalBoxArr);
+        $this->assertEquals('Jost Verstappen', $postalBoxArr['recipientName']);
+        $this->assertEquals('jv@example.com', $postalBoxArr['recipientEmail']);
+        $this->assertEquals('1234567891', $postalBoxArr['recipientPhone']);
+        $this->assertEquals('KR203', $postalBoxArr['postalBox']);
+    }
+
+    public function test_postal_box_delivery_created_by_static_make()
+    {
+        $postalBox = TransactionWizard\Delivery\PostalBox::make(
+            'jv@example.com',
+            'Jost Verstappen',
+            '1234567891',
+            'KR203'
+        );
+
+        $postalBoxArr = $postalBox->toArray();
+        $this->assertIsArray($postalBoxArr);
+        $this->assertEquals('Jost Verstappen', $postalBoxArr['recipientName']);
+        $this->assertEquals('jv@example.com', $postalBoxArr['recipientEmail']);
+        $this->assertEquals('1234567891', $postalBoxArr['recipientPhone']);
+        $this->assertEquals('KR203', $postalBoxArr['postalBox']);
     }
 
     public function test_address_postal_box_definition()
@@ -200,7 +323,6 @@ class TransactionWizardTest extends TestCase
             '600500200',
             $this->transaction->toArray()['buyer']['delivery']['recipientPhone']
         );
-
 
 
         $this->assertStringContainsString(
