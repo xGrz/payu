e7f28707d6345e384e53b8af35fa4dbae5ed9d92
xGrz <33755434+xGrz@users.noreply.github.com>
2024-05-08 11:11:44 +0200

    Refund index blade refactored

--- resources/views/refunds/index.blade.php
+++ resources/views/refunds/index.blade.php
@@ -2,45 +2,45 @@
 @extends('p::app')
 
 @section('content')
-    <x-p::pagination.info :source="$refunds"/>
-    <x-p::paper class="bg-slate-800">
-        <x-p::paper-title title="Created refunds"/>
+    <x-p-pagination info-only :source="$refunds"/>
+    <x-p-paper>
+        <x-slot:title>Created refunds</x-slot:title>
         @if($refunds->count())
-            <x-p::table class="w-full">
-                <x-p::table.head>
-                    <x-p::table.row>
-                        <x-p::table.th class="text-left">Transaction</x-p::table.th>
-                        <x-p::table.th class="text-left">Description</x-p::table.th>
-                        <x-p::table.th class="text-left">Bank description</x-p::table.th>
-                        <x-p::table.th class="text-right">Amount</x-p::table.th>
-                        <x-p::table.th class="text-right">Status</x-p::table.th>
-                    </x-p::table.row>
-                </x-p::table.head>
-                <x-p::table.body>
+            <x-p-table class="w-full">
+                <x-p-table>
+                    <x-p-tr>
+                        <x-p-th class="text-left">Transaction</x-p-th>
+                        <x-p-th class="text-left">Description</x-p-th>
+                        <x-p-th class="text-left">Bank description</x-p-th>
+                        <x-p-th class="text-right">Amount</x-p-th>
+                        <x-p-th class="text-right">Status</x-p-th>
+                    </x-p-tr>
+                </x-p-table>
+                <x-p-tbody>
                     @foreach($refunds as $refund)
-                        <x-p::table.row>
-                            <x-p::table.cell>
-                                <x-p::link
+                        <x-p-tr>
+                            <x-p-td>
+                                <x-p-link
                                     href="{{route(Config::getRouteName('payments.show'), $refund->transaction->id)}}">
                                     {{$refund->transaction->payload['description']}}
-                                </x-p::link>
-                            </x-p::table.cell>
-                            <x-p::table.cell>{{$refund->description}}</x-p::table.cell>
-                            <x-p::table.cell>{{$refund->bankDescription}}</x-p::table.cell>
-                            <x-p::table.cell class="text-right">{{ humanAmount($refund->amount)}}</x-p::table.cell>
-                            <x-p::table.cell class="text-right">
-                                <x-p::status :status="$refund->status"/>
-                            </x-p::table.cell>
-                        </x-p::table.row>
+                                </x-p-link>
+                            </x-p-td>
+                            <x-p-td>{{$refund->description}}</x-p-td>
+                            <x-p-td>{{$refund->bankDescription}}</x-p-td>
+                            <x-p-td class="text-right">{{ humanAmount($refund->amount)}}</x-p-td>
+                            <x-p-td class="text-right">
+                                <x-p-status :status="$refund->status"/>
+                            </x-p-td>
+                        </x-p-tr>
                     @endforeach
-                </x-p::table.body>
-            </x-p::table>
+                </x-p-tbody>
+            </x-p-table>
             <div class="py-2">
-                <x-p::pagination :source="$refunds"/>
+                <x-p-pagination :source="$refunds"/>
             </div>
         @else
-            <x-p::not-found message="Refunds not found"/>
+            <x-p-not-found message="Refunds not found"/>
         @endif
 
-    </x-p::paper>
+    </x-p-paper>
 @endsection
