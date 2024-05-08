338c5fa5052620c0ac8730793b91e91f8abaf8a7
xGrz <33755434+xGrz@users.noreply.github.com>
2024-05-08 11:08:40 +0200

    Transaction show refactored (qbp).

--- resources/views/transactions/partials/transaction_details.blade.php
+++ resources/views/transactions/partials/transaction_details.blade.php
@@ -1,6 +1,7 @@
 @php use \xGrz\PayU\Facades\Config; @endphp
-<x-p::paper class="bg-slate-800">
-    <x-p::paper-title title="Transaction details">
+<x-p-paper class="bg-slate-800">
+    <x-slot:title>Transaction details</x-slot:title>
+    <x-slot:actions>
         <form action="{{route(Config::getRouteName('payments.accept'), $transaction->id)}}" method="POST" id="accept">
             @method('PATCH')
             @csrf
@@ -11,45 +12,45 @@
         </form>
 
         @if($transaction->status->hasAction('delete'))
-            <x-p::button form="reject" type="submit" color="danger">Delete transaction</x-p::button>
+            <x-p-button form="reject" type="submit" color="danger">Delete transaction</x-p-button>
         @endif
 
         @if($transaction->status->hasAction('accept'))
-            <x-p::button form="accept" type="submit" color="success">Accept</x-p::button>
+            <x-p-button form="accept" type="submit" color="success">Accept</x-p-button>
         @endif
         @if($transaction->status->hasAction('reject'))
-            <x-p::button form="reject" type="submit" color="danger">Reject</x-p::button>
+            <x-p-button form="reject" type="submit" color="danger">Reject</x-p-button>
         @endif
-    </x-p::paper-title>
-    <x-p::table class="p-2">
-        <x-p::table.row>
-            <x-p::table.th class="text-left">Transaction id</x-p::table.th>
-            <x-p::table.cell>{{$transaction->id}}</x-p::table.cell>
-        </x-p::table.row>
-        <x-p::table.row>
-            <x-p::table.th class="text-left">PayU order id</x-p::table.th>
-            <x-p::table.cell>{{$transaction->payu_order_id}}</x-p::table.cell>
-        </x-p::table.row>
-        <x-p::table.row>
-            <x-p::table.th class="text-left">Payment link</x-p::table.th>
-            <x-p::table.cell>
-                <x-p::link href="{!! $transaction->link !!}">Payment link</x-p::link>
-            </x-p::table.cell>
-        </x-p::table.row>
-        <x-p::table.row>
-            <x-p::table.th class="text-left">Status</x-p::table.th>
-            <x-p::table.cell>
-                <x-p::status :status="$transaction->status" class="text-lg py-1 px-2"/>
-            </x-p::table.cell>
-        </x-p::table.row>
-        <x-p::table.row>
-            <x-p::table.th class="text-left">PayMethod</x-p::table.th>
-            <x-p::table.cell>
+    </x-slot:actions>
+    <x-p-table class="p-2">
+        <x-p-tr>
+            <x-p-th class="text-left">Transaction id</x-p-th>
+            <x-p-td>{{$transaction->id}}</x-p-td>
+        </x-p-tr>
+        <x-p-tr>
+            <x-p-th class="text-left">PayU order id</x-p-th>
+            <x-p-td>{{$transaction->payu_order_id}}</x-p-td>
+        </x-p-tr>
+        <x-p-tr>
+            <x-p-th class="text-left">Payment link</x-p-th>
+            <x-p-td>
+                <x-p-link href="{!! $transaction->link !!}">Payment link</x-p-link>
+            </x-p-td>
+        </x-p-tr>
+        <x-p-tr>
+            <x-p-th class="text-left">Status</x-p-th>
+            <x-p-td>
+                <x-p-status :status="$transaction->status" class="text-lg py-1 px-2"/>
+            </x-p-td>
+        </x-p-tr>
+        <x-p-tr>
+            <x-p-th class="text-left">PayMethod</x-p-th>
+            <x-p-td>
                 @if (empty($transaction->payMethod))
                     No transaction details received yet.
                     @if($transaction->status->hasAction('payMethod'))
-                        <x-p::link href="{{ route('payu.payments.method', $transaction->id) }}">Get pay method now.
-                        </x-p::link>
+                        <x-p-link href="{{ route('payu.payments.method', $transaction->id) }}">Get pay method now.
+                        </x-p-link>
                     @endif
                 @else
                     {{ $transaction->payMethod->name }}
@@ -59,33 +60,33 @@
                             class="max-h-10 max-w-16 fill bg-white p-2"
                     />
                 @endif
-            </x-p::table.cell>
-        </x-p::table.row>
-        <x-p::table.row>
-            <x-p::table.th class="text-left">Amount</x-p::table.th>
-            <x-p::table.cell>{{ humanAmount($transaction->payload['totalAmount'] / 100, $transaction->payload['currencyCode']) }}</x-p::table.cell>
-        </x-p::table.row>
-        <x-p::table.row>
-            <x-p::table.th class="text-left">Refunded</x-p::table.th>
-            <x-p::table.cell>
+            </x-p-td>
+        </x-p-tr>
+        <x-p-tr>
+            <x-p-th class="text-left">Amount</x-p-th>
+            <x-p-td>{{ humanAmount($transaction->payload['totalAmount'] / 100, $transaction->payload['currencyCode']) }}</x-p-td>
+        </x-p-tr>
+        <x-p-tr>
+            <x-p-th class="text-left">Refunded</x-p-th>
+            <x-p-td>
                 {{ humanAmount($transaction->refundedAmount(), $transaction->payload['currencyCode']) }}
-            </x-p::table.cell>
-        </x-p::table.row>
-        <x-p::table.row>
-            <x-p::table.th class="text-left">Description</x-p::table.th>
-            <x-p::table.cell>{{ $transaction->payload['description'] }}</x-p::table.cell>
-        </x-p::table.row>
+            </x-p-td>
+        </x-p-tr>
+        <x-p-tr>
+            <x-p-th class="text-left">Description</x-p-th>
+            <x-p-td>{{ $transaction->payload['description'] }}</x-p-td>
+        </x-p-tr>
         @if (!empty($transaction->payload['additionalDescription']) && $transaction->payload['additionalDescription'] !== $transaction->payload['description'])
-            <x-p::table.row>
-                <x-p::table.th class="text-left">Additional description</x-p::table.th>
-                <x-p::table.cell>{{ $transaction->payload['additionalDescription'] }}</x-p::table.cell>
-            </x-p::table.row>
+            <x-p-tr>
+                <x-p-th class="text-left">Additional description</x-p-th>
+                <x-p-td>{{ $transaction->payload['additionalDescription'] }}</x-p-td>
+            </x-p-tr>
         @endif
         @if (!empty($transaction->payload['visibleDescription']) && $transaction->payload['visibleDescription'] !== $transaction->payload['description'])
-            <x-p::table.row>
-                <x-p::table.th class="text-left">User visible description</x-p::table.th>
-                <x-p::table.cell>{{ $transaction->payload['visibleDescription'] }}</x-p::table.cell>
-            </x-p::table.row>
+            <x-p-tr>
+                <x-p-th class="text-left">User visible description</x-p-th>
+                <x-p-td>{{ $transaction->payload['visibleDescription'] }}</x-p-td>
+            </x-p-tr>
         @endif
-    </x-p::table>
-</x-p::paper>
+    </x-p-table>
+</x-p-paper>
--- resources/views/transactions/partials/transaction_refunds.blade.php
+++ resources/views/transactions/partials/transaction_refunds.blade.php
@@ -2,67 +2,70 @@
 @props(['tableTitle' => '[tableTitle] not set', 'shouldRenderAction' => false])
 
 @if ($transaction->status->hasAction('refund'))
-    <x-p::paper class="bg-slate-800 mt-4">
-        <x-p::paper-title title="{{ $tableTitle ?? 'Upss, no title set' }}">
-            @if($shouldRenderAction && $transaction->isRefundAvailable())
+    <x-p-paper class="bg-slate-800 mt-4">
+        <x-slot:title>{{ $tableTitle ?? 'Upss, no title set' }}</x-slot:title>
+
+        @if($shouldRenderAction && $transaction->isRefundAvailable())
+            <x-slot:actions>
                 @include('payu::refunds.create-refund')
-            @endif
-        </x-p::paper-title>
+            </x-slot:actions>
+        @endif
 
         @if ($transaction->refunds->count())
-            <x-p::table>
-                <x-p::table.head>
-                    <x-p::table.row>
-                        <x-p::table.th class="text-left">Description</x-p::table.th>
-                        <x-p::table.th>RefundId</x-p::table.th>
-                        <x-p::table.th class="text-left">Bank description</x-p::table.th>
-                        <x-p::table.th class="text-left">Status</x-p::table.th>
-                        <x-p::table.th class="text-right">Amount</x-p::table.th>
-                        <x-p::table.th class="text-right">Date</x-p::table.th>
+            <x-p-table>
+                <x-p-thead>
+                    <x-p-tr>
+                        <x-p-th class="text-left">Description</x-p-th>
+                        <x-p-th>RefundId</x-p-th>
+                        <x-p-th class="text-left">Bank description</x-p-th>
+                        <x-p-th class="text-left">Status</x-p-th>
+                        <x-p-th class="text-right">Amount</x-p-th>
+                        <x-p-th class="text-right">Date</x-p-th>
                         <td></td>
-                    </x-p::table.row>
-                </x-p::table.head>
+                    </x-p-tr>
+                </x-p-thead>
                 <tbody>
                 @foreach($transaction->refunds as $refund)
-                    <x-p::table.row>
-                        <x-p::table.cell>{{ $refund->description }}</x-p::table.cell>
-                        <x-p::table.cell>{{ $refund->refund_id }}</x-p::table.cell>
-                        <x-p::table.cell>{{ $refund->bank_description }}</x-p::table.cell>
-                        <x-p::table.cell>
-                            <x-p::status :status="$refund->status"/>
+                    <x-p-tr>
+                        <x-p-td>{{ $refund->description }}</x-p-td>
+                        <x-p-td>{{ $refund->refund_id }}</x-p-td>
+                        <x-p-td>{{ $refund->bank_description }}</x-p-td>
+                        <x-p-td>
+                            <x-p-status :status="$refund->status"/>
                             @if($refund->error)
                                 <small class="block">{{ $refund->errorDescription }}</small>
                             @endif
-                        </x-p::table.cell>
-                        <x-p::table.cell
-                            class="text-right">{{ humanAmount($refund->amount, $refund->currency_code) }}</x-p::table.cell>
-                        <x-p::table.cell class="text-right">{{ $refund->created_at }}</x-p::table.cell>
-                        <x-p::table.cell class="text-right">
+                        </x-p-td>
+                        <x-p-td
+                            class="text-right">{{ humanAmount($refund->amount, $refund->currency_code) }}</x-p-td>
+                        <x-p-td class="text-right">{{ $refund->created_at }}</x-p-td>
+                        <x-p-td class="text-right">
                             @if($refund->status->hasAction('delete'))
-                                <form action="{{ route(Config::getRouteName('refunds.destroy'), $refund->id) }}" method="POST"
+                                <form action="{{ route(Config::getRouteName('refunds.destroy'), $refund->id) }}"
+                                      method="POST"
                                       id="delete_refund_{{$refund->id}}">
                                     @csrf @method('DELETE')
                                 </form>
-                                <x-p::button class="text-red-500" form="delete_refund_{{$refund->id}}" size="small"
-                                                color="danger" type="submit">Delete
-                                </x-p::button>
+                                <x-p-button class="text-red-500" form="delete_refund_{{$refund->id}}" size="small"
+                                             color="danger" type="submit">Delete
+                                </x-p-button>
                             @endif
                             @if($refund->status->hasAction('retry'))
-                                <x-p::buttonlink href="{{route(Config::getRouteName('refunds.retry'), $refund->id)}}">
+                                <x-p-button href="{{route(Config::getRouteName('refunds.retry'), $refund->id)}}">
                                     Retry
-                                </x-p::buttonlink>
+                                </x-p-button>
                             @endif
-                        </x-p::table.cell>
-                    </x-p::table.row>
+                        </x-p-td>
+                    </x-p-tr>
                 @endforeach
                 </tbody>
-            </x-p::table>
+            </x-p-table>
             <div class="text-gray-500 my-1 mx-2">
                 Once refund is sent you cannot revoke it.
             </div>
         @else
-            <x-p::not-found message="Refunds not found"/>
+            <x-p-not-found message="Refunds not found"/>
         @endif
 
-    </x-p::paper>
+    </x-p-paper>
 @endif
