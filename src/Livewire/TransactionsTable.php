c59fde969e2363ca6bcf3e8dfce0f812f506f602
xGrz <33755434+xGrz@users.noreply.github.com>
2024-05-08 20:24:15 +0200

    Added livewire pagination to transactions table

--- src/Livewire/TransactionsTable.php
+++ src/Livewire/TransactionsTable.php
@@ -3,10 +3,13 @@
 namespace xGrz\PayU\Livewire;
 
 use Livewire\Component;
+use Livewire\WithPagination;
 use xGrz\PayU\Models\Transaction;
 
 class TransactionsTable extends Component
 {
+    use WithPagination;
+
     public function render()
     {
         return view('payu::transactions.transactions-table', [
